10e45c35586328854a7414debd0b695ced306c6d
Vlad Chekunov <gwellir@gmail.com>
2020-08-13 20:28:30 +0300

    Fix featured block a bit more.

--- src/public/js/FeaturedPanels.js
+++ src/public/js/FeaturedPanels.js
@@ -5,13 +5,12 @@ const featuredPanels = {
     `<div class="featured-new padding-site">
         <div class="feature-menu-wrapper clearfix">
             <ul class="featured-menu">
-                <li class="featured-list"><a href="#" class="featured-link featured-link-active">Featured</a></li>
-                <li class="featured-list"><a href="#" class="featured-link">New</a></li>
+                <li v-for="mode in modes" @click="setMode(mode)" class="featured-list"><div :class="classPerCategory(mode)">{{mode}}</div></li>
             </ul>
             <router-link to="/products" class="explore-link">Explore all <img class="text-arrow" src="img/arrow.svg" alt=""></router-link>
         </div>
         <div class="featured-panels">
-            <div v-for="item in feat_list" class="panel-small featured">
+            <div v-for="item in filtered" class="panel-small featured">
                 <img :src="item.img">
                 <item-overlay :item="item"></item-overlay>
             </div>
@@ -26,16 +25,36 @@ const featuredPanels = {
     data() {
         return {
             maxItems: 4,
+            mode: 'Featured',
+            modes: [
+                'Featured',
+                'New'
+            ]
         }
     },
     methods: {
-        showOverlay() {}
+        classPerCategory(cat) {
+            let classes = "featured-link";
+            if (cat === this.mode) {
+                classes += " featured-link-active"
+            }
+            return classes;
+        },
+        setMode(mode) {
+            if (mode !== this.mode) {
+                this.mode = mode
+            }
+        }
     },
     computed: {
-        feat_list() {
-            return this.items
-                .filter(item => item.type === "featured")
-                .slice(0, this.maxItems);
+        filtered() {
+            if (this.mode === 'Featured') {
+                return this.items
+                    .filter(item => item.type === 'featured')
+                    .slice(0, this.maxItems);
+            } else if (this.mode === 'New') {
+                return this.items.slice(this.items.length - this.maxItems, this.items.length);
+            }
         }
     }
 };
