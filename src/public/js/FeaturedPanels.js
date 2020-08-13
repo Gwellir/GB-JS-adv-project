1c52a6959026d90beb70ed50c8c9d7001e24a11a
Vlad Chekunov <gwellir@gmail.com>
2020-08-13 20:10:20 +0300

    Change featured block layout

--- src/public/js/FeaturedPanels.js
+++ src/public/js/FeaturedPanels.js
@@ -2,11 +2,20 @@ import itemOverlay from "./ItemOverlay";
 
 const featuredPanels = {
     template: 
-    `<div class="featured-panels">
-    <div v-for="item in feat_list" class="panel-small featured">
-        <img :src="item.img">
-        <item-overlay :item="item"></item-overlay>
-    </div>
+    `<div class="featured-new padding-site">
+        <div class="feature-menu-wrapper clearfix">
+            <ul class="featured-menu">
+                <li class="featured-list"><a href="#" class="featured-link featured-link-active">Featured</a></li>
+                <li class="featured-list"><a href="#" class="featured-link">New</a></li>
+            </ul>
+            <router-link to="/products" class="explore-link">Explore all <img class="text-arrow" src="img/arrow.svg" alt=""></router-link>
+        </div>
+        <div class="featured-panels">
+            <div v-for="item in feat_list" class="panel-small featured">
+                <img :src="item.img">
+                <item-overlay :item="item"></item-overlay>
+            </div>
+        </div>
     </div>`,
     components: {
         'item-overlay': itemOverlay
